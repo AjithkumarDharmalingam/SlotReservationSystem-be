ecf6c74499fcd5519014be37807e197ac1062301
AjithkumarDharmalingam <ajithkumarbe97@gmail.com>
2023-12-21 15:51:32 +0530

    last commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5001;
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://extraordinary-crumble-394c7a.netlify.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   })
